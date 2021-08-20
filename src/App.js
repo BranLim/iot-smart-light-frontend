3aa86e6a1da87d05f555babdc1ad78b948543d27
Brandon Lim <brandon.lim@zuhlke.com>
2021-08-20 13:22:08 +0800

    Add more styling.

--- src/App.js
+++ src/App.js
@@ -1,56 +1,76 @@
 import './App.css';
 import React, {useState} from 'react';
 import {
-    Container,
     Box,
-    FormControl,
+    Container,
     FormControlLabel,
-    Switch,
-    Typography,
     FormGroup,
-    FormLabel, Slider
+    FormLabel,
+    Grid,
+    Slider,
+    Switch,
+    Typography
 } from '@material-ui/core';
-import {withStyles} from "@material-ui/core/styles";
 
 function App() {
 
     const [lightState, setLightState] = useState(0);
 
     return (
-        <Container maxWidth="sm">
+        <Container maxWidth="md">
             <Typography variant="h2">
                 Smart Light Control Panel
             </Typography>
-            <Box my={6}>
+            <Box ml={2} pt={4}>
                 <form>
                     <FormGroup>
-                        <FormLabel>Name</FormLabel>
-                    </FormGroup>
-                    <br/>
-                    <FormGroup>
+                        <Grid container spacing={2}>
+                            <Grid item sm={3}>
+                                <FormLabel>Name</FormLabel>
+                            </Grid>
+                            <Grid>
 
-                        <FormLabel htmlFor="lightSwitch">Current Status</FormLabel>
-                        <FormControlLabel id="lightSwitch" margin="normal" fullwidth="true" margin="normal"
-                                          control={
-                                              <Switch onChange={(evt) => {
-                                                  evt.target.checked ? setLightState(1) : setLightState(0);
-                                              }}/>
-                                          }
-                                          label={lightState === 1 ? "On" : "Off"} labelPlacement="end"/>
+                            </Grid>
+                        </Grid>
+
                     </FormGroup>
                     <br/>
                     <FormGroup>
-                        <FormLabel>Brightness</FormLabel>
-                        <Slider defaultValue={10}
-                                aria-labelledby="discrete-slider-small-steps"
-                                step={2}
-                                marks
-                                min={1}
-                                max={100}
-                                valueLabelDisplay="auto"></Slider>
+                        <Grid container spacing={2}>
+                            <Grid item sm={3}>
+                                <FormLabel htmlFor="lightSwitch">Current Status</FormLabel>
+                            </Grid>
+                            <Grid item sm={9}>
+                                <FormControlLabel id="lightSwitch" margin="normal" fullwidth="true" margin="normal"
+                                                  control={
+                                                      <Switch onChange={(evt) => {
+                                                          evt.target.checked ? setLightState(1) : setLightState(0);
+                                                      }}/>
+                                                  }
+                                                  label={lightState === 1 ? "On" : "Off"} labelPlacement="end"/>
+                            </Grid>
+                        </Grid>
+                    </FormGroup>
+                    <FormGroup>
+                        <Grid container spacing={2}>
+                            <Grid item sm={3}>
+                                <FormLabel>Brightness</FormLabel>
+                            </Grid>
+                            <Grid item sm={9}>
+                                <Slider defaultValue={10}
+                                        aria-labelledby="discrete-slider-small-steps"
+                                        step={2}
+                                        marks
+                                        min={1}
+                                        max={100}
+                                        valueLabelDisplay="auto"></Slider>
+                            </Grid>
+                        </Grid>
+
                     </FormGroup>
                 </form>
             </Box>
+
         </Container>
     );
 }
